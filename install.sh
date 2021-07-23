ff176c2992d1ad8be55154f4f1c589be0cb94fd3
Vishnu Vamsee Paningipalli <62656098+vamseep36@users.noreply.github.com>
2021-07-23 20:12:20 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install python3
+sudo apt-get install python-psycopg2
+sudo apt-get install awscli
+sudo apt-get install azure-cli
+sudo apt-get install google-cloud-sdk
+pip install -r requirements.txt 
